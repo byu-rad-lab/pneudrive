a2acfab310fc076767801cd451bdc94a7c3cf110
Demo User <ubuntu@beaglebone.localdomain>
2020-04-22 23:36:01 +0000

    I think this install script works, but only for pressure control, not IMU publishing too

--- beaglebone_setup/bb_install_script.sh
+++ beaglebone_setup/bb_install_script.sh
@@ -2,18 +2,18 @@
 
 # INSTALL ROS and needed packages
 
-#sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
-#sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 
 sudo apt-get update
 
-#sudo apt install ros-melodic-ros-base -y
+sudo apt install ros-melodic-ros-base -y
 
-#echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source /opt/ros/melodic/setup.bash
 
-#sudo apt-get install nano ros-melodic-robot-upstart ros-melodic-tf -y
+sudo apt-get install nano ros-melodic-robot-upstart ros-melodic-tf -y
 
 # Set IP Address on boot
 sudo cp set_ip_on_startup /etc/init.d/
@@ -30,6 +30,8 @@ git clone https://bitbucket.org/byu_rad_lab/byu_pressure_control.git
 cd ~/ros_ws
 catkin_make
 source devel/setup.bash
+echo "source ~/ros_ws/devel/setup.bash" >> ~/.bashrc
+
 
 # Enable I2C on the BeagleBoneBlack
 
@@ -37,9 +39,9 @@ echo "uboot_overlay_addr6=/lib/firmware/BB-I2C1-FAST-00A0.dtbo" >> /boot/uEnv.tx
 echo "uboot_overlay_addr7=/lib/firmware/BB-I2C2-FAST-00A0.dtbo" >> /boot/uEnv.txt
 
 # Launch pressure control and/or IMU on boot
-rosrun robot_upstart install byu_pressure_control/launch/i2c_imu.launch --user root --job imu_publisher
-sudo systemctl daemon-reload 
-sudo systemctl start imu_publisher
+#rosrun robot_upstart install byu_pressure_control/launch/i2c_imu.launch --user root --job imu_publisher
+#sudo systemctl daemon-reload 
+#sudo systemctl start imu_publisher
 
 
 rosrun robot_upstart install byu_pressure_control/launch/i2c_pressure.launch --user root --job pressure_control
--- beaglebone_setup/setIPOnStartup.py
+++ beaglebone_setup/setIPOnStartup.py
@@ -3,4 +3,5 @@ import os
 import time
 
 time.sleep(1.0)
+os.system('mkdir /home/ubuntu/phildif')
 os.system('ifconfig eth0 192.168.0.188 netmask 255.255.255.0')
