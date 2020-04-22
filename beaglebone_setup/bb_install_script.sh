213b37f9a0ec5535bc019022084f5ad19eef5e1f
Phil Hyatt <phyatt16@gmail.com>
2020-04-22 14:29:46 -0600

    Trying out a new install script

--- beaglebone_setup/bb_install_script.sh
+++ beaglebone_setup/bb_install_script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# INSTALL ROS and needed packages
+
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
@@ -13,6 +15,19 @@ source ~/.bashrc
 
 apt-get install nano ros-melodic-robot-upstart ros-melodic-tf -y
 
+# Create a ROS Workspace and put this package in it
+cd
+mkdir -p ~/ros_ws/src
+cd ros_ws
+catkin_make
+cd src
+git clone https://bitbucket.org/byu_rad_lab/byu_pressure_control.git
+cd ..
+catkin_make
+
+# Enable I2C on the BeagleBoneBlack
+
 echo "uboot_overlay_addr6=/lib/firmware/BB-I2C1-FAST-00A0.dtbo" >> /boot/uEnv.txt
 
 echo "uboot_overlay_addr7=/lib/firmware/BB-I2C2-FAST-00A0.dtbo" >> /boot/uEnv.txt
+
