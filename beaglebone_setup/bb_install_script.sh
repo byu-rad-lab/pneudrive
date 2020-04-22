5aa3b461d9f314e20afb01701c9a5911190b2d43
Phil Hyatt <phyatt16@gmail.com>
2020-04-22 15:23:05 -0600

    Added setting ip on startup and launching imu and pressure control on boot  Changes to be committed: 	modified:   bb_install_script.sh 	new file:   set_ip_on_startup

--- beaglebone_setup/bb_install_script.sh
+++ beaglebone_setup/bb_install_script.sh
@@ -11,19 +11,22 @@ apt-get update
 sudo apt install ros-melodic-ros-base -y
 
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
+source /opt/ros/melodic/setup.bash
 
 apt-get install nano ros-melodic-robot-upstart ros-melodic-tf -y
 
+# Set IP Address on boot
+cp set_ip_on_startup /etc/init.d/
+
 # Create a ROS Workspace and put this package in it
-cd
-mkdir -p ~/ros_ws/src
-cd ros_ws
-catkin_make
-cd src
-git clone https://bitbucket.org/byu_rad_lab/byu_pressure_control.git
 cd ..
-catkin_make
+rm -rf byu_pressure_control/
+cd ~/
+mkdir -p ~/ros_ws/src
+cd ros_ws/src
+git clone https://bitbucket.org/byu_rad_lab/byu_pressure_control.git
+cd ~/
+sudo chown -R ubuntu .
 
 # Enable I2C on the BeagleBoneBlack
 
@@ -31,3 +34,7 @@ echo "uboot_overlay_addr6=/lib/firmware/BB-I2C1-FAST-00A0.dtbo" >> /boot/uEnv.tx
 
 echo "uboot_overlay_addr7=/lib/firmware/BB-I2C2-FAST-00A0.dtbo" >> /boot/uEnv.txt
 
+# Launch pressure control and/or IMU on boot
+rosrun robot_upstart install byu_pressure_control/launch/i2c_imu.launch --user root
+
+rosrun robot_upstart install byu_pressure_control/launch/i2c_pressure.launch --user root
