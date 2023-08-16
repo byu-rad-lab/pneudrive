34044c42e1df91087256935dadb439553a7a12d7
Curtis Johnson <curtiscarljohnson@gmail.com>
2023-08-16 20:48:59 +0000

    arduino_code.ino edited online with Bitbucket. Working call response. Needs to be integrated into i2c code and cleaned up

--- RS485_code/arduino_code/arduino_code.ino
+++ RS485_code/arduino_code/arduino_code.ino
@@ -8,7 +8,7 @@ unsigned char pressure_command_bytes[8]; //comes from odroid
 unsigned short pressure_command_shorts[4];
 // unsigned short pressure_state_shorts[4] = {222, 333, 444, 555}; 
 unsigned short pressure_state_shorts[4] = {666, 777, 888, 999};
-unsigned char pressure_state_bytes[9]; // sends to odroid
+unsigned char pressure_state_bytes[9] = {ADDRESS, 0x02, 0x9A, 0x03, 0x09, 0x03, 0x78, 0x03, 0xE7}; // sends to odroid
 
 void byteToShorts(unsigned short *short_array, unsigned char *byte_array) {
   //Function to convert array of 8 bytes to array of 4 shorts
@@ -35,6 +35,7 @@ void shortToBytes(unsigned short *short_array, unsigned char *byte_array) {
 void setup() {
   Serial1.begin(1000000);  // RS485 Serial port
   pressure_state_bytes[0] = ADDRESS;
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 
@@ -42,9 +43,10 @@ void setup() {
 //  Main
 //===============================================================================
 void loop() {
-  if (Serial1.available()) // If data has come in from Odroid
+  if (Serial1.available() == 9) // If data has come in from Odroid
   {
-    if (Serial1.read() == ADDRESS) {
+    if (Serial1.read() == ADDRESS) 
+    {
       // if address is correct, read in the rest of the message (8 bytes)
       for (int i=0; i<8; i++) {
         pressure_command_bytes[i] = Serial1.read();
@@ -53,18 +55,30 @@ void loop() {
       //convert the bytes to shorts to be used by the arduino
       byteToShorts(pressure_command_shorts, pressure_command_bytes);
 
-      // do something with the pressure commands here
+//      // do something with the pressure commands here
+//      if (pressure_command_shorts[0] = 43690)
+//      {
+//        digitalWrite(LED_BUILTIN, HIGH);
+//      }
       
-      // still not sure why, but this is the smallest delay that allows the odroid to receive data
-      delayMicroseconds(25);
-
+//      // still not sure why, but this is the smallest delay that allows the odroid to receive data
+// delay from rs485 boards is 120 us, and packet takes 90 us (120-90 = 30 us), but sending to RX buffer takes about 30 us. so add just a bit.
+      delayMicroseconds(5);
+//
       // convert the current state into bytes
-      shortToBytes(pressure_state_shorts, pressure_state_bytes);
+//      shortToBytes(pressure_state_shorts, pressure_state_bytes);
 
 
       // Send current state back to odroid
       for (int i=0; i<9; i++) {
         Serial1.write(pressure_state_bytes[i]);
+      }
+    }
+    else
+    {
+      while (Serial1.available())
+      {
+        Serial1.read();
       }
     }
   }
