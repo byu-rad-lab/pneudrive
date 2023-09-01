3f1e14988a4b1246c88a4a20af45ebf953b6382f
Curtis <curtiscarljohnson@gmail.com>
2023-09-01 13:52:48 -0600

    platform io push

---
+++ arduino_code/pressure_control/src/main.cpp
@@ -0,0 +1,347 @@
+#include "A4990ValveInterface.h"
+
+/*
+  NOTE ON VALVE COMMAND SIGNALS
+  -----------------------------
+  Valve PWM commands are on the interval [-400,400].
+
+  -400 corresponds to commanding -1 amp (vent) and 400 corresponds to +1 amp (fill).
+  The driver boards actively limit current to 0.7 amps however,
+  so any command above 0.7 amps will result in 0.7 amps on the hardware. See current2PWM() function below.
+
+  Negative numbers mean vent and positive mean fill.
+  If the valve is flipped, the wires need to be switched.
+
+*/
+
+/*
+  IMPORTANT NOTE ON TIMER FREQUENCIES
+  ------------------------------------
+  On the Arduino Nano Every (ATmega 4809 chipset), there are two timers related to the PWM pins we
+  using: TCA for D5 and D9 and TCB for D3 and D6.
+
+  The default pwm freq for these pins is 975 Hz = fclk_per/64 (where fclk_per is the peripheral clock,
+  and is set to 62.4kHz by default. This freq causes a really annoying hum when the valves are being
+  controlled. To get rid of this, we prescale the timers to 31.2kHz = fclk_per/2 in speedupPWM() because
+  31.2 kHz is well outside the range of what a human can hear.
+
+  TCB can be changed like this without causing any issues. TCA, on the other hand, is used by the
+  built-in delay() and millis() functions. Since we changed the freq by a factor of 32 (PRESCALER),
+  this means that millis() now reports 32 times the actual time passed (e.g. millis()/PRESCALER is the
+  actual number of milliseconds that have passed).
+
+  Another consequence of changing TCA is that delay() now (for some reason) can only delay up to
+  32,768 (2^16/2) which corresponds to an actual delay of 32768/32 = 1024 ms. So, there is a custom
+  delay function defined custom_delay() which allows you to delay for longer than 1024 ms. Since
+  delay() function isn't used in the control loop, this is adequate for our needs.
+*/
+
+// ANYWHERE WE SEE FLOAT, CHANGE IT
+
+A4990ValveInterface valves;
+
+unsigned short p[4] = {0, 0, 0, 0};
+unsigned short pcmd[4] = {0, 0, 0, 0};
+
+// Keep, make it explicitly a short
+short valve_cmd[4] = {0, 0, 0, 0};
+short VENT_CMD[4] = {-400, -400, -400, -400};
+short FILL_CMD[4] = {400, 400, 400, 400};
+
+// Probably move to odroid. We just want integer math
+// conversion factor from psi to kpa (ie kpa = psi * PSI2KPA)
+double const PSI2KPA = 6.8947572932;
+double const P_MAX = 100 * PSI2KPA;
+
+// IGNORE
+//'Global' variables that are important for control
+// only proportional control b/c these dynamics are essentially a stable first order system and I don't
+// want to add a destabilizing integrator on this low level control
+float myTime = 0.0;
+float prevTime = 0.0; // Last time the loop was entered
+
+/*  int bus;
+   Sensors in this configuration have noise magnitude of about 0.5 psi.
+   Making railPSI < 2 made the arm very jittery.
+   railPSI=2 seems to track well.
+   railPSI>5 was pretty sluggish tracking and seemed to exacerbate valve nonlinearities with small commands.
+
+*/
+float railPSI = 2;
+float kp = 0.7 / (railPSI * PSI2KPA); // this kp means desired current will rail at .7 amps at a pressure error of >= railPSI.
+
+// denominator is set by the pressure error which will cause the input to saturate.
+// KEEP DEADBAND
+short deadband = 0; // kpa, controller will not act on error less than deadband
+short error = 0;
+
+// timer stuff (KEEP)
+#define PRESCALER 32
+#define ONE_SECOND 32000
+
+#define BYTES_IN_PACKET 9
+
+byte outgoing_bytes[BYTES_IN_PACKET] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+byte incoming_bytes[BYTES_IN_PACKET] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+char rs485_address = 'z';
+unsigned short pressure_commands[4] = {0, 0, 0, 0};
+unsigned short pressure_data[4] = {1, 2, 3, 4};
+
+void byteToShorts(unsigned short *short_array, const byte *byte_array)
+{
+  // Function to convert array of 8 bytes to array of 4 shorts, ignoring first byte
+  unsigned int byteLength = 8;
+  for (size_t i = 0; i < byteLength; i += 2)
+  {
+    int byteIndex = i + 1;
+    short_array[i / 2] = ((short)byte_array[byteIndex] << 8) | byte_array[byteIndex + 1];
+  }
+}
+
+void shortToBytes(const unsigned short *short_array, byte *byte_array)
+{
+  // Function to convert array of 4 shorts to array of 8 bytes (doesn't change first byte because of address byte)
+  int shortLength = 4;
+  for (size_t i = 0; i < shortLength; i++)
+  {
+    int byteIndex = i * 2 + 1;
+    unsigned char *bytePtr = (unsigned char *)&short_array[i];
+    byte_array[byteIndex] = bytePtr[1];     // Most significant byte
+    byte_array[byteIndex + 1] = bytePtr[0]; // Least significant byte
+  }
+}
+
+void handleIncomingBytes()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  if (Serial1.available() == 9) // specifically wait for correct amount before trying to read
+  {
+    Serial1.readBytes(incoming_bytes, BYTES_IN_PACKET);
+
+    if (static_cast<char>(incoming_bytes[0]) == rs485_address)
+    {
+      // digitalWrite(LED_BUILTIN, HIGH);
+      // respond with pressure data
+
+      Serial1.write(outgoing_bytes, BYTES_IN_PACKET);
+
+      // if the incoming array was meant for this device, save it for use in control
+      byteToShorts(pressure_commands, incoming_bytes);
+    }
+  }
+}
+
+void readPressureData()
+{
+  pressure_data[0] = analogRead(A0);
+  pressure_data[1] = analogRead(A1);
+  pressure_data[2] = analogRead(A2);
+  pressure_data[3] = analogRead(A3);
+}
+
+//
+// float filter(float prev, float input)
+//{
+//  /*
+//      This function implements a first order low pass filter
+//      with a cutoff frequency of 50 Hz. First order hold discrete implementation with dt=.001.
+//      First order filter is of form:
+//      a / (z - b)
+//  */
+//
+//  float a = .2696;
+//  float b = .7304;
+//
+//  return b * prev + a * input(v_out - 0.1 * v_sup) / (0.8 * v_sup)) * P_MAX;
+//}
+
+int getrs485address()
+{
+  /* Addresses from dip switch
+      a number indicates a DIP switch flipped ON
+     PIN |Addr
+     ---------
+     _ _ = 0
+     1 _ = 1
+     _ 1 = 2
+     1 1 = 3
+
+     Note that when the DIP switch is on, the actual voltage is LOW. When it is OFF,
+     the voltage is pulled HIGH.
+  */
+
+  int rs485addr;
+  int one;
+  int two;
+
+  one = digitalRead(10);
+  two = digitalRead(11);
+
+  if (one == HIGH && two == HIGH)
+  {
+    rs485addr = 'a';
+  }
+  else if (one == LOW && two == HIGH)
+  {
+    rs485addr = 'b';
+  }
+  else if (one == HIGH && two == LOW)
+  {
+    rs485addr = 'c';
+  }
+  else if (one == LOW && two == LOW)
+  {
+    rs485addr = 'd';
+  }
+  return rs485addr;
+}
+
+int current2PWM(double current)
+{
+  /*
+    From enfield valve measurements: current = .004*PWM
+
+    Note, this model is only valid in the linear regime which is PWM=[-175,175].
+    Outside of this, the current is maxed out by the driver boards at ~0.7 amps.
+    That is, any PWM command outside of [-175,175] will be saturated at .7 amps.
+  */
+  int PWM = int(current / .004);
+  return PWM;
+}
+// LEAVE
+void speedupPWM()
+{
+  cli(); // Disable Interrupts
+
+  /* see section 20.5.1 of http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-4809-Data-Sheet-DS40002173A.pdf
+     for this register settings. Note that by default, fclk_per = 62.4 kHz.
+     Humans can hear 20 Hz to 20 kHz, so a 31.2kHz freq should be out of hearing range.
+     Also see section 5.4.1 for syntax used here.
+     | is "bitwise or" commonly used to set multiple bits at once in a register.
+     TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm --> 0,0,1,0 OR 0,0,0,1 = 0,0,1,1
+
+     READ TIMER FREQUENCY NOTE AT TOP
+  */
+
+  // TCB for D3 and D6
+  TCB0.CTRLA = (TCB_CLKSEL_CLKDIV2_gc) | (TCB_ENABLE_bm);
+  // TCA for D5 and D9
+  TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm;
+
+  sei(); // Enable Interrupts
+}
+// LEAVE
+void custom_delay(int seconds)
+{
+  for (int s = 0; s < seconds; s++)
+  {
+    delay(ONE_SECOND);
+  }
+}
+
+//===============================================================================
+//  Initialization
+//===============================================================================
+void setup()
+{
+
+  // b/c we are using pin 13 as an input, the LED_BUILTIN cannot be used.
+  pinMode(10, INPUT_PULLUP);
+  pinMode(11, INPUT_PULLUP);
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+
+  speedupPWM();
+  analogReference(EXTERNAL);
+
+  // comment out if not debugging
+  // Serial.begin(9600);
+
+  rs485_address = getrs485address();
+  // Serial.println(rs485_address);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial1.begin(1000000); // RS485 Serial port
+  outgoing_bytes[0] = rs485_address;
+
+  //  // uncomment to test valves
+  //  valves.setValve0Speed(-200);
+  //  valves.setValve1Speed(-200);
+  //  valves.setValve2Speed(-200);
+  //  valves.setValve3Speed(-200);
+
+  // vent all valves at first
+  valves.setSpeeds(VENT_CMD);
+
+  // Wait for 5 seconds for everything to vent out.
+  custom_delay(5);
+
+  // close all valves now that they have vented.
+  valves.setSpeeds(valve_cmd);
+}
+
+//===============================================================================
+//  Main
+//===============================================================================
+void loop()
+{
+
+  readPressureData(); // expensive...
+  shortToBytes(pressure_data, outgoing_bytes);
+  handleIncomingBytes();
+
+  //  // KEEP, WILL CHANGE LATER, CHECK INTS, MOVE READ PRESSURE STUFF TO ODROID
+  //  // digital filter pressure data
+  //  p[0] = filter(p[0], A0);
+  //  p[1] = filter(p[1], A1);
+  //  p[2] = filter(p[2], A2);
+  //  p[3] = filter(p[3], A3);
+  //
+  //  //  //Uncomment to plot filtered data vs unfiltered data
+  //  //  Serial.print(myTime);
+  //  //  Serial.print("\t");
+  //  //  Serial.print(readPressure(A0));
+  //  //  Serial.print("\t");
+  //  //  Serial.print(p[0]);
+  //  //  Serial.print("\t");
+  //  //  Serial.print(fir_lp.processReading(readPressure(A0)));
+  //  //  Serial.println();
+  //
+  //  //  // Uncomment to see all pressures plotted together on serial monitor
+  //  //  for (int i = 0; i < 4; i++) {
+  //  //    Serial.print(p[0]);
+  //  //    Serial.print(",");
+  //  //  }
+  //  //  Serial.println();
+  //
+  //  // KEEP THIS FOR NOW
+  //  // PROPORTIONAL CONTROL on each valve
+  //  for (int i = 0; i < 4; i++)
+  //  {
+  //    // calculate control signals for each pressure
+  //    if (abs(pcmd[i] - p[i]) >= deadband)
+  //    {
+  //      error = pcmd[i] - p[i];
+  //
+  //      valve_cmd[i] = current2PWM(kp * error);
+  //
+  //      //      if (i == 0) {
+  //      //        Serial.print(valve_cmd[i]);
+  //      //        Serial.println();
+  //      //      }
+  //    }
+  //  }
+  //
+  //  // send updated control signals [-400,400]
+  //  valves.setSpeeds(valve_cmd);
+  //
+  //  // update p and pcmd in memory with new values of pchar and pcmdchar recieved on rs485
+  //  for (int i = 0; i < 4; i++)
+  //  {
+  //    shortToBytes(p[i], &pchar[i * BYTES_PER_PRESSURE]);
+  //    byteToShorts(pcmd[i], &pcmdchar[i * BYTES_PER_PRESSURE]);
+  //  }
+}
