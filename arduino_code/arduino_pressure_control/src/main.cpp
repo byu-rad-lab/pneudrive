3cda9b22b579f60e17b54daac41af49b042bdccd
Curtis <curtiscarljohnson@gmail.com>
2023-09-20 09:09:36 -0600

    code clean up

--- arduino_code/arduino_pressure_control/src/main.cpp
+++ arduino_code/arduino_pressure_control/src/main.cpp
@@ -38,29 +38,17 @@
 
 A4990ValveInterface valves;
 
-// Keep, make it explicitly a short
 short valve_cmd[4] = {0, 0, 0, 0};
 short VENT_CMD[4] = {-400, -400, -400, -400};
 short FILL_CMD[4] = {400, 400, 400, 400};
 
-/*  int bus;
-   Sensors in this configuration have noise magnitude of about 0.5 psi.
-   Making railPSI < 2 made the arm very jittery.
-   railPSI=2 seems to track well.
-   railPSI>5 was pretty sluggish tracking and seemed to exacerbate valve nonlinearities with small commands.
-
-*/
-// denominator is set by the pressure error which will cause the input to saturate.
 const unsigned short MAX_INPUT = 400;
 unsigned short saturation_error = 20; // TUNE ME, lower is more aggressive :)
 unsigned short kp = MAX_INPUT / saturation_error;
 
-// timer stuff (KEEP)
 #define PRESCALER 32
 #define ONE_SECOND 32000
-
 #define BYTES_IN_PACKET 10
-
 #define DEBUG_MODE false
 
 byte outgoingBytes[BYTES_IN_PACKET] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -72,9 +60,9 @@ unsigned short rs485_address = 0x0000;
 unsigned short pressure_commands[4] = {0, 0, 0, 0};
 unsigned short pressure_data[4] = {0, 0, 0, 0};
 
+// Function to convert array of 8 bytes to array of 4 shorts
 void byteToShorts(unsigned short *short_array, const byte *byte_array)
 {
-  // Function to convert array of 8 bytes to array of 4 shorts
   unsigned int byteLength = 8;
   for (size_t i = 0; i < byteLength; i += 2)
   {
@@ -94,16 +82,16 @@ void byteToShorts(unsigned short *short_array, const byte *byte_array)
   }
 }
 
+// Function to convert array of 5 shorts to array of 10 bytes, but in big endian
 void shortToBytes(const unsigned short *short_array, byte *byte_array)
 {
-  // Function to convert array of 5 shorts to array of 10 bytes, but in big endian
   int shortLength = 5;
   for (size_t i = 0; i < shortLength; i++)
   {
     int byteIndex = i * 2;
     unsigned char *bytePtr = (unsigned char *)&short_array[i];
 
-    // convert from little endian to big endian for odroid
+    // Convert from little endian to big endian
     byte_array[byteIndex] = bytePtr[1];     // LSB
     byte_array[byteIndex + 1] = bytePtr[0]; // MSB
   }
@@ -130,9 +118,6 @@ void handleIncomingBytes()
       Serial.print("Available bytes: ");
       Serial.println(Serial1.available(), DEC);
     }
-    // read first 2 bytes of transmission to see if this device is being addressed
-    // if address is never found, this will simply empty the RX buffer.
-
     byte secondByte = Serial1.read();
 
     if (DEBUG_MODE)
@@ -143,7 +128,7 @@ void handleIncomingBytes()
       Serial.println(secondByte, DEC);
     }
 
-    unsigned short short1 = (firstByte << 8) | secondByte; // arduino is little endian
+    unsigned short short1 = (firstByte << 8) | secondByte;
 
     if (short1 == rs485_address)
     {
@@ -152,7 +137,6 @@ void handleIncomingBytes()
         Serial.println("Address found!");
       }
 
-      // if this device address is found, save the next 8 bytes because they contain pressure commands sent from controller
       size_t numBytesRead = Serial1.readBytes(incomingDataBytes, BYTES_IN_PACKET - 2);
 
       if (DEBUG_MODE)
@@ -169,7 +153,7 @@ void handleIncomingBytes()
       if (numBytesRead == 8)
       {
         Serial1.flush();
-        if (Serial1.availableForWrite() == SERIAL_TX_BUFFER_SIZE - 1) // if tx buffer is empty
+        if (Serial1.availableForWrite() == SERIAL_TX_BUFFER_SIZE - 1)
         {
           size_t numBytesWritten = Serial1.write(outgoingBytes, BYTES_IN_PACKET);
           if (DEBUG_MODE)
@@ -178,10 +162,9 @@ void handleIncomingBytes()
             Serial.println(numBytesWritten, DEC);
           }
         }
-        // if the incoming array was meant for this device, save it for use in control
         byteToShorts(pressure_commands, incomingDataBytes);
 
-        firstByte = 0; // reset to scan for address of next packet
+        firstByte = 0;
       }
       else
       {
@@ -203,8 +186,6 @@ void handleIncomingBytes()
 
 void readPressureData()
 {
-  // input voltage range from pressure sensors is .1Vsup - .9Vsup, which is .5V to 4.5V
-  // These voltages correspond to about 102 and 921 on the arduino analog input.
   for (int i = 0; i < 4; i++)
   {
     pressure_data[i] = analogRead(A0 + i);
@@ -233,19 +214,6 @@ void custom_delay(int seconds)
 
 int getrs485address()
 {
-  /* Addresses from dip switch
-      a number indicates a DIP switch flipped ON
-     PIN |Addr
-     ---------
-     _ _ = 0
-     1 _ = 1
-     _ 1 = 2
-     1 1 = 3
-
-     Note that when the DIP switch is on, the actual voltage is LOW. When it is OFF,
-     the voltage is pulled HIGH.
-  */
-
   unsigned short rs485addr;
   int one;
   int two;
@@ -277,34 +245,27 @@ int getrs485address()
 
 void speedupPWM()
 {
+  /* see section 20.5.1 of http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-4809-Data-Sheet-DS40002173A.pdf
+    for this register settings. Note that by default, fclk_per = 62.4 kHz.
+    Humans can hear 20 Hz to 20 kHz, so a 31.2kHz freq should be out of hearing range.
+    Also see section 5.4.1 for syntax used here.
+    | is "bitwise or" commonly used to set multiple bits at once in a register.
+    TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm --> 0,0,1,0 OR 0,0,0,1 = 0,0,1,1
+
+    READ TIMER FREQUENCY NOTE AT TOP
+*/
   cli(); // Disable Interrupts
 
-  /* see section 20.5.1 of http://ww1.microchip.com/downloads/en/DeviceDoc/ATmega4808-4809-Data-Sheet-DS40002173A.pdf
-     for this register settings. Note that by default, fclk_per = 62.4 kHz.
-     Humans can hear 20 Hz to 20 kHz, so a 31.2kHz freq should be out of hearing range.
-     Also see section 5.4.1 for syntax used here.
-     | is "bitwise or" commonly used to set multiple bits at once in a register.
-     TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm --> 0,0,1,0 OR 0,0,0,1 = 0,0,1,1
-
-     READ TIMER FREQUENCY NOTE AT TOP
-  */
-
-  // TCB for D3 and D6
+  // Set timer frequencies to 31.2 kHz
   TCB0.CTRLA = (TCB_CLKSEL_CLKDIV2_gc) | (TCB_ENABLE_bm);
-  // TCA for D5 and D9
   TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_DIV2_gc | TCA_SINGLE_ENABLE_bm;
 
   sei(); // Enable Interrupts
 }
 
-//===============================================================================
-//  Initialization
-//===============================================================================
 void setup()
 {
-
   pinMode(LED_BUILTIN, OUTPUT);
-
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
@@ -323,7 +284,7 @@ void setup()
   }
 
   Serial1.begin(1000000); // RS485 Serial port
-  Serial1.setTimeout(1);  // set timeout to 1 ms
+  Serial1.setTimeout(1);
 
   outgoingShorts[0] = rs485_address;
 
@@ -332,12 +293,9 @@ void setup()
   custom_delay(5);
 }
 
-//===============================================================================
-//  Main
-//===============================================================================
 void loop()
 {
-  readPressureData(); // expensive... 300-500 us, about half of the loop time I think
+  readPressureData();
   shortToBytes(outgoingShorts, outgoingBytes);
   handleIncomingBytes();
 
